8615dcd03d27922a6fbea4d38587c9220dd0cf7c
DouglasTeles <douglastelesoliveira@gmail.com>
2021-07-18 23:32:29 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 
-//Users
+//Client
 const ClienteController = require('./app/controllers/ClienteController')
 router.post('/cadastro',ClienteController.store) //Cadastrar um cliente
-router.get('/clientes',ClienteController.index) //LISTA DE CLIENTES
-router.put('/cliente/:id/update',ClienteController.update)//Atualiza um Cliente
-router.delete('/cliente/:id/delete', ClienteController.delete)//Deleta um cliente
+router.get('/clientes',ClienteController.index) //Visualizar Clientes
+router.put('/cliente/:id/update',ClienteController.update)//Atualizar  Cliente
+router.delete('/cliente/:id/delete', ClienteController.delete)//Deletar cliente
 
 
 module.exports = router
